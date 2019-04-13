9075dedcbaab75c5c2f2674d067aef6ff5bea4e5
Daniel Wang <hello@danielwang.dev>
2019-04-13 14:25:58 -0400

    style: use std::uintptr_t instead of uintptr_t

--- csgo-external/sdk/entity.cpp
+++ csgo-external/sdk/entity.cpp
@@ -65,8 +65,8 @@ auto entity_t::is_valid() -> bool
 
 auto entity_t::get_class_id() -> int
 {
-	uintptr_t vtable = remote::read<std::uintptr_t>(base + 0x8); // IClientNetworkable
-	uintptr_t fn = remote::read<std::uintptr_t>(vtable + 0x8); // 4 bytes per fn, 2nd index
-	uintptr_t ptr = remote::read<std::uintptr_t>(fn + 0x1); // mov eax, ptr_to_client_class
+	std::uintptr_t vtable = remote::read<std::uintptr_t>(base + 0x8); // IClientNetworkable
+	std::uintptr_t fn = remote::read<std::uintptr_t>(vtable + 0x8); // 4 bytes per fn, 2nd index
+	std::uintptr_t ptr = remote::read<std::uintptr_t>(fn + 0x1); // mov eax, ptr_to_client_class
 	return remote::read<int>(ptr + 0x14);
 }
